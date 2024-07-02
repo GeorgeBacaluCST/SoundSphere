29ee9c840bdc95669898c6ebd594362b86fb3a5b
GeorgeBacaluCST <george.bacalu@cst.ro>
2024-07-02 08:36:16 +0300

    Configure database connection

--- SoundSphere.Api/Program.cs
+++ SoundSphere.Api/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SoundSphere.Database.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SoundSphereDbContext>(options => options.UseSqlServer(
+    string.Format(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new Exception("Missing connection string"),
+                  Environment.GetEnvironmentVariable("SQLSERVER_HOST"),
+                  Environment.GetEnvironmentVariable("SQLSERVER_USERID"),
+                  Environment.GetEnvironmentVariable("SQLSERVER_PASSWORD")),
+    sqlOptions => sqlOptions.MigrationsAssembly("SoundSphere.Api")));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ SoundSphere.Database/Context/SoundSphereDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoundSphere.Database.Entities;
+
+namespace SoundSphere.Database.Context
+{
+    public class SoundSphereDbContext : DbContext
+    {
+        public SoundSphereDbContext(DbContextOptions<SoundSphereDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
